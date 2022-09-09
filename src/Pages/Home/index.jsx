024cad24ba2c6990aad6ee0e646627039ab65637
Carlos <carlosemaya@gmail.com>
2022-09-09 20:01:36 -0300

    Add and remove students

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,21 +1,50 @@
+import { useState } from 'react';
 import './styles.css';
 import { Plus, Trash } from 'phosphor-react';
 import { CardUser } from '../../components/CardUser'
 
 export function Home() {
+    const [studentName, setStudentName] = useState('');
+    const [students, setStudents] = useState([])
+
+
+    function handleAddStudent() {
+        const newStudent = {
+            name: studentName,
+        }
+
+        setStudents(prevState => [...prevState, newStudent]);
+
+        setStudentName('');
+    }
+
+    function handleRemoveStudent(idStudent) {
+        setStudents(students.filter((e, index) => index !== idStudent));
+    }
+
     return (
         <div className="container">
             <h1>Lista de alunos do curso</h1>
             <div className="input-container">
-                <input type="text" placeholder="Nome do Aluno"/>
-                <button title="Adicionar Aluno">
+                <input 
+                    type="text" 
+                    placeholder="Nome do Aluno"
+                    onChange={e => setStudentName(e.target.value)}
+                    value={studentName}    
+                />
+                <button title="Adicionar Aluno" onClick={handleAddStudent}>
                     <Plus size={26} weight="bold" color='#FFF'/>
                 </button>
             </div>
             <div className="container-list">
-                <CardUser name="Carlos"/>
-                <CardUser name="Leticia"/>
-                <CardUser name="Lucas"/>
+                {
+                    students.map((student, index) => 
+                        <CardUser key={index} 
+                        name={student.name} 
+                        idStudent={index} 
+                        onDeleteStudent={handleRemoveStudent} />
+                    )
+                }
             </div>
         </div>
     )
--- src/components/CardUser/index.jsx
+++ src/components/CardUser/index.jsx
@@ -2,10 +2,15 @@ import { Plus, Trash } from 'phosphor-react';
 import './styles.css'
 
 export function CardUser(props) {
+
+    function deleteUser(idUser){
+        props.onDeleteStudent(idUser);
+    }
+
     return(
         <div className='container-card'>
             <p>{props.name}</p>
-            <button>
+            <button onClick={() => deleteUser(props.idStudent)}>
                 <Trash size={32} weight="bold" color='#FFF' />
             </button>
         </div>
